c51c426e6822918d131472ede659b0a6839775ee
jfrejlac <jfrejlac@redhat.com>
2021-11-25 12:27:27 +0100

    Init test folder

---
+++ tests/test_core.py
@@ -0,0 +1,11 @@
+import pytest
+import logging
+
+logger = logging.getLogger(__name__)
+pytestmark = pytest.mark.unit
+
+
+class TestCore(object):
+    def test_test(self):
+        """test the tests ..."""
+        assert 1 == 1
