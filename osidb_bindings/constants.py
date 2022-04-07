9e1cad7796fc5cb83d5e0bda5b896d8bb1afeb98
Jakub Frejlach <jfrejlac@redhat.com>
2022-04-07 10:49:56 +0200

    Preparation of 1.1.1 release

--- osidb_bindings/constants.py
+++ osidb_bindings/constants.py
@@ -1,5 +1,5 @@
 OSIDB_API_VERSION: str = "v1"
-OSIDB_BINDINGS_USERAGENT: str = "osidb-bindings-1.1.0"
+OSIDB_BINDINGS_USERAGENT: str = "osidb-bindings-1.1.1"
 OSIDB_BINDINGS_API_PATH: str = ".bindings.python_client.api.osidb"
 
 TRUSTED_CAS: str = "/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt"
