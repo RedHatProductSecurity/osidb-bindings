67b31a84e9fa1e1556022650e6c25654c481a690
Jakub Frejlach <jfrejlac@redhat.com>
2023-06-28 13:17:12 +0200

    Preparation of 3.3.0 release

--- osidb_bindings/constants.py
+++ osidb_bindings/constants.py
@@ -1,7 +1,7 @@
 from typing import List
 
 OSIDB_API_VERSION: str = "v1"
-OSIDB_BINDINGS_VERSION: str = "3.1.0"
+OSIDB_BINDINGS_VERSION: str = "3.3.0"
 OSIDB_BINDINGS_USERAGENT: str = f"osidb-bindings-{OSIDB_BINDINGS_VERSION}"
 OSIDB_BINDINGS_API_PATH: str = ".bindings.python_client.api.osidb"
 
