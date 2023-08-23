c458e738abec0ea503b41f8b87ed30421ffc1309
Jakub Frejlach <jfrejlac@redhat.com>
2023-08-23 09:40:57 +0200

    Preparation of 3.4.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="osidb-bindings",
-    version="3.4.0",
+    version="3.4.1",
     author="Jakub Frejlach, Red Hat Product Security",
     author_email="jfrejlac@redhat.com",
     description="Python bindings for accessing OSIDB API",
