bc250886efef8d4ea1b28f97f8a685531a7dfb84
Jakub Frejlach <jfrejlac@redhat.com>
2024-12-19 18:03:26 +0100

    Improve code formatting and linting
    * switch out black,isort and flake8 for ruff
    * update tox
    * reformat/lint/sort imports to comply with ruff settings

--- osidb_bindings/__init__.py
+++ osidb_bindings/__init__.py
@@ -1,4 +1,5 @@
-from .session import Paginator, new_session
+from .session import Paginator
+from .session import new_session
 from .utils import cve_ids
 
 __all__ = ("cve_ids", "new_session", "Paginator")
