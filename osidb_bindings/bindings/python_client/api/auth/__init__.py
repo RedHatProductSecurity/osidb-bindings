80ca867db036372e2248c4f113ecba7387a5ab5c
Jakub Frejlach <jfrejlac@redhat.com>
2025-11-01 09:18:17 +0100

    Regenerate bindings for new templates

--- osidb_bindings/bindings/python_client/api/auth/__init__.py
+++ osidb_bindings/bindings/python_client/api/auth/__init__.py
@@ -3,3 +3,11 @@ from .auth_token_refresh_create import *
 from .auth_token_refresh_retrieve import *
 from .auth_token_retrieve import *
 from .auth_token_verify_create import *
+
+ENDPOINT_NAMES = [
+    "auth_token_retrieve",
+    "auth_token_create",
+    "auth_token_refresh_retrieve",
+    "auth_token_refresh_create",
+    "auth_token_verify_create",
+]
